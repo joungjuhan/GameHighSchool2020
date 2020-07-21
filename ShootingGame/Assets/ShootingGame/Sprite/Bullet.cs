6e06e9ff1daf2b0ec7b39bcbdf1c6560a6db3e48
joungjuhan <66407910+joungjuhan@users.noreply.github.com>
2020-07-21 19:01:36 +0900

    슈팅게임 1회차

---
+++ ShootingGame/Assets/ShootingGame/Sprite/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float m_Speed = 2f;
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 velocity = transform.up* m_Speed; //방향 *시간
+        Vector3 movement = velocity * Time.deltaTime;  
+        transform.position += movement;
+        //        transform.position += transform.up * m_Speed * Time.deltaTime;
+       //  Vector3 movement = transform.up * m_Speed * Time.deltaTime;
+    }
+}
---
+++ ShootingGame/Assets/ShootingGame/Sprite/playercontroll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class playercontroll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * 5f * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * 5f * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * 5f * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {   
+            transform.position += Vector3.left * 5f * Time.deltaTime;
+        }
+
+    }
+}
