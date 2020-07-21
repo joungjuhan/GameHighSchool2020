300c12cf066691d1c7004f4fe42ef9cfe6e3ad39
joungjuhan <66407910+joungjuhan@users.noreply.github.com>
2020-07-21 21:24:34 +0900

    슈팅게임 2회차ㅈㅊ

--- ShootingGame/Assets/ShootingGame/Sprite/Bullet.cs
+++ ShootingGame/Assets/ShootingGame/Sprite/Bullet.cs
@@ -10,13 +10,19 @@ public class Bullet : MonoBehaviour
         
     }
     public float m_Speed = 2f;
+
+
     // Update is called once per frame
     private void Update()
     {
-        Vector3 velocity = transform.up* m_Speed; //방향 *시간
-        Vector3 movement = velocity * Time.deltaTime;  
-        transform.position += movement;
-        //        transform.position += transform.up * m_Speed * Time.deltaTime;
-       //  Vector3 movement = transform.up * m_Speed * Time.deltaTime;
+        
+            Vector3 velocity = transform.up * m_Speed; //방향 *시간
+            Vector3 movement = velocity * Time.deltaTime;
+            transform.position += movement;
+            //        transform.position += transform.up * m_Speed * Time.deltaTime;
+            //  Vector3 movement = transform.up * m_Speed * Time.deltaTime;
+        
+      
+
     }
 }
--- ShootingGame/Assets/ShootingGame/Sprite/playercontroll.cs
+++ ShootingGame/Assets/ShootingGame/Sprite/playercontroll.cs
@@ -8,9 +8,14 @@ public class playercontroll : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
+    public float m_Speed = 1f;
+    public Bullet m_bullet;
+    public float m_AttackDelay = 0.5f;
+    public float m_AttackCooldown = 0f;
+
+    public Transform[] m_FireMuzzle;
     // Update is called once per frame
     void Update()
     {
@@ -30,9 +35,20 @@ public class playercontroll : MonoBehaviour
 
         }
         if (Input.GetKey(KeyCode.A))
-        {   
+        {
             transform.position += Vector3.left * 5f * Time.deltaTime;
         }
 
+
+        if (Input.GetKey(KeyCode.R) && m_AttackCooldown <= 0)
+        {
+            foreach (var FireMuzzle in m_FireMuzzle)
+            {
+                var bullet = GameObject.Instantiate(m_bullet, FireMuzzle.position, FireMuzzle.rotation);
+            }
+            m_AttackCooldown = m_AttackDelay;
+
+        }
+        m_AttackCooldown -= Time.deltaTime;
     }
 }
