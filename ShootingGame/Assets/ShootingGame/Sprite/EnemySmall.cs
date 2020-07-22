d39522e7afbb6c7daf1a7e903ff0b9a4b145baf8
joungjuhan <66407910+joungjuhan@users.noreply.github.com>
2020-07-22 21:00:13 +0900

    슈딩게이임3회차

--- ShootingGame/Assets/ShootingGame/Sprite/EnemySmall.cs
+++ ShootingGame/Assets/ShootingGame/Sprite/EnemySmall.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class EnemySmall : MonoBehaviour
@@ -10,10 +11,40 @@ public class EnemySmall : MonoBehaviour
     {
         
     }
+    public Bullet m_bullet;
+    
 
+    public Transform[] m_FireMuzzle;
+    public float m_AttackDelay = 0.5f;
+    public float m_AttackCooldown = 0f;
+
+    public bool isDead = false;
     // Update is called once per frame
     void Update()
     {
         transform.position -= transform.up * 2f * Time.deltaTime;
+        if (!isDead && m_AttackCooldown <= 0)
+        {
+            foreach (var FireMuzzle in m_FireMuzzle)
+            {
+                var bullet = GameObject.Instantiate(m_bullet, FireMuzzle.position, FireMuzzle.rotation);
+                m_AttackCooldown = m_AttackDelay;
+            }
+            m_AttackCooldown -= Time.deltaTime;
+        }
+
+    }
+    public Animator m_Animator;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "PlayerBullet")
+        {
+            m_Animator.SetBool("Die", true);
+            isDead = true;
+        }
+    }
+    public void Die()
+    {
+        Destroy(gameObject);
     }
 }
