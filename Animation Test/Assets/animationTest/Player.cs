035202282964ff9dd9d3b9ff920146eedbf74a0b
joungjuhan <66407910+joungjuhan@users.noreply.github.com>
2020-07-23 21:17:15 +0900

    애니메이션 연습

---
+++ Animation Test/Assets/animationTest/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public Animator m_animator;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            m_animator.SetTrigger("Attack");
+        }
+
+    }
+}
