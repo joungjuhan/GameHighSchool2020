71a75141e80813a053810ec78ee72ebda60d92f4
joungjuhan <66407910+joungjuhan@users.noreply.github.com>
2020-09-02 21:10:23 +0900

    서니랜드 3회차

--- AfterSchoolTest/Sunny Land/Assets/GameSchool/Scripts/PlayerController.cs
+++ AfterSchoolTest/Sunny Land/Assets/GameSchool/Scripts/PlayerController.cs
@@ -12,10 +12,13 @@ public class PlayerController : MonoBehaviour
     public float m_XAxisSpeed = 3f;
     public float m_YJumpPower = 3f;
     public float m_JumpCount = 0f;
+    public float m_ClimbSpeed = 2f;
 
     public bool Jumpbool;
     public bool m_IsClimbing = false;
     public bool m_IsJumping = false;
+    public bool m_m_Isclimbing = false;
+    public bool m_IsTouchLadder = false;
     private Animator m_Animator;
     protected void Start()
     {
@@ -28,43 +31,75 @@ public class PlayerController : MonoBehaviour
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
 
-        Vector2 velocity = m_Rigidbody2D.velocity;
-        velocity.x = xAxis * m_XAxisSpeed;
-        m_Rigidbody2D.velocity = velocity;
-        //ㅈㅍ
-        if(Jumpbool)
+
+        if(m_IsTouchLadder == true && Mathf.Abs(yAxis) > 0.5f)
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            m_IsClimbing = true;
+        }
+
+        if(!m_IsClimbing)
+        {
+            Vector2 velocity = m_Rigidbody2D.velocity;
+            velocity.x = xAxis * m_XAxisSpeed;
+            m_Rigidbody2D.velocity = velocity;
+            //ㅈㅍ
+            if (Jumpbool)
             {
-                m_Rigidbody2D.AddForce(Vector3.up * m_YJumpPower);
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    m_Rigidbody2D.AddForce(Vector3.up * m_YJumpPower);
 
-                Jumpbool = false;
+                }
             }
-        }
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            m_Rigidbody2D.AddForce(Vector3.down * m_YJumpPower);
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                m_Rigidbody2D.AddForce(Vector3.down * m_YJumpPower);
+
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                m_Rigidbody2D.AddForce(Vector3.down * -yAxis);
+            }
+
+            //캐릭터 스프라이트 방향따라가기
+            if (xAxis > 0)
+                transform.localScale = new Vector3(1, 1, 1);
+            if (xAxis < 0)
+                transform.localScale = new Vector3(-1, 1, 1);
+
+            var animator = GetComponent<Animator>();
+            animator.SetFloat("Velocity", velocity.y);
+
+            m_IsJumping = Mathf.Abs(velocity.y) >= 0.1f ? true : false;
+
+            m_Animator.SetBool("isJumping", m_IsJumping);
+            m_Animator.SetFloat("VelocityX", Mathf.Abs(xAxis));
+            m_Animator.SetFloat("VelocityY", velocity.y);
 
         }
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            m_Rigidbody2D.AddForce(Vector3.down * -yAxis);
-        }
+        else
+                {
+                    m_Rigidbody2D.constraints = m_Rigidbody2D.constraints 
+                    | RigidbodyConstraints2D.FreezePosition;
+                Vector3 movement = Vector3.zero;
+                movement.x = xAxis * Time.deltaTime;
+                movement.y = yAxis * Time.deltaTime;
+            transform.position += movement;
 
-        //캐릭터 스프라이트 방향따라가기
-        if (xAxis > 0)
-            transform.localScale = new Vector3(1,1,1);
-        if (xAxis < 0)
-            transform.localScale = new Vector3(-1,1,1);
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                ClimbExit();
+            }
+            m_Animator.SetBool("IsClimbing", m_IsClimbing);
+            m_Animator.SetFloat("ClimbSpeed", Mathf.Abs(xAxis) + Mathf.Abs(yAxis));
+                }
+    }
+    private void ClimbExit()
+    {
+        m_Rigidbody2D.constraints = m_Rigidbody2D.constraints & ~RigidbodyConstraints2D.FreezePosition;
+        m_IsClimbing = false;
 
-        var animator = GetComponent<Animator>();
-        animator.SetFloat("VelocityY", velocity.y);
-
-        m_IsJumping = Mathf.Abs(velocity.y) >= 0.1f ? true : false;
-
-        m_Animator.SetBool("IsJumping", m_IsJumping);
-        m_Animator.SetFloat("Velocity", Mathf.Abs(xAxis));
-        m_Animator.SetFloat("Velocity", velocity.y);
+        m_Animator.SetBool("IsClimbing", m_IsClimbing);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -73,10 +108,27 @@ public class PlayerController : MonoBehaviour
         {
             Jumpbool = true;
         }
-        if (collision.gameObject.tag == "ladder")
-        {
-            Jumpbool = false;
-        }
+     
 
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Ladder")
+        {
+            m_IsTouchLadder = true;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag =="Ladder")
+        {
+            m_IsTouchLadder = false;
+
+            ClimbExit();
+        }
+    }
+
 }
