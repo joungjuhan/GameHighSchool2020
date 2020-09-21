37b52d83e9867a693396836aa38385215b8c28b7
joungjuhan <66407910+joungjuhan@users.noreply.github.com>
2020-09-21 21:11:57 +0900

    고디스바니아 1회차

---
+++ AfterSchoolTest/GothisVania/Assets/makeGothisVania/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xAxis= Input.GetAxis("Horizontal");//좌우입력
+
+        float sMovement= speed * xAxis * Time.deltaTime;
+        Vector3 movement = Vector3.right * sMovement;
+        transform.position += movement;
+
+    }
+
+}
