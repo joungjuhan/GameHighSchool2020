bb33cbfc1e3f70fefd2a281228423c0e8e933e15
joungjuhan <66407910+joungjuhan@users.noreply.github.com>
2020-10-12 21:09:42 +0900

    서니랜드우드 2,3,4

---
+++ AfterSchoolTest/SunnyLandWoods/Assets/Scripts/EventTrigger2D.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventTrigger2D : MonoBehaviour
+{
+    [System.Flags]
+    public enum Mask
+    {
+        None = 0,
+            Player = (1 << 0 ),
+            Enemy = (1<<1),
+            Ect = (1<<2),
+            All = Player| Enemy,
+    }
+
+    public Mask m_Mask;
+    public UnityEvent m_OnTriggerEnter;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if((m_Mask & Mask.Player) == Mask.Player)
+        {
+            if (collision.attachedRigidbody.tag == "Player")
+                m_OnTriggerEnter.Invoke();
+        }
+    }
+
+
+}
